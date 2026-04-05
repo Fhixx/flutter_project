35ceb54048063e5aa41fb83e0ce070b12b7043f1
Fhixx <17taufiknh@gmail.com>
2026-04-05 11:08:05 +0700

    PB-03 Manajemen Stok Barang

---
+++ Manajer/flutter_application_manager/lib/features/addstock/services/stock_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class StockService {
+  static const String baseUrl = "http://10.0.2.2:8000";
+
+  static Future<bool> addStock({
+    required int productId,
+    required int qty,
+    required String tipe,
+    required String keterangan,
+  }) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/stock/add"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "product_id": productId,
+        "qty": qty,
+        "tipe": tipe,
+        "keterangan": keterangan,
+      }),
+    );
+
+     print("STATUS: ${response.statusCode}");
+    print("BODY: ${response.body}");
+    
+    return response.statusCode == 200;
+  }
+}
